206aef8c88938df1efa1f4b246203b324057bb03
daihere1993 <daihere1993@gmail.com>
2022-10-30 11:29:36 +0800

    backup

--- algorithm/demo.cpp
+++ algorithm/demo.cpp
@@ -15,24 +15,17 @@ struct ListNode {
 
 class Solution {
 public:
-    vector<vector<int>> twoSum(vector<int>& nums, int start, int end, int target) {
-      vector<vector<int>> ret;
+    vector<int> twoSum(vector<int>& nums, int target) {
+      vector<int> ret;
+      int start = 0;
+      int end = nums.size() - 1;
+      sort(nums.begin(), nums.end());
       while (start < end) {
         int sum = nums[start] + nums[end];
         if (sum == target) {
-          vector<int> v;
-          v.push_back(-target);
-          v.push_back(nums[start]);
-          v.push_back(nums[end]);
-          ret.push_back(v);
-          start++;
-          end--;
-          while (start < end && nums[start] == nums[start-1]) {
-            start++;
-          }
-          while (start < end && nums[end] == nums[end+1]) {
-            end--;
-          }
+          ret.push_back(start);
+          ret.push_back(end);
+          return ret;
         } else if (sum < target) {
           start++;
         } else {
@@ -41,27 +34,13 @@ public:
       }
       return ret;
     }
-    // 1. 对数组排序，避免出现重复的结果
-    // 2. 将 threeSum 转化为 twoSum 的问题
-    vector<vector<int>> threeSum(vector<int>& nums) {
-      vector<vector<int>> ret;
-      sort(nums.begin(), nums.end());
-      for (int i = 0; i < nums.size(); i++) {
-        if (i > 0 && nums[i] == nums[i-1]) {
-          continue;
-        }
-        auto v = twoSum(nums, i+1, nums.size()-1, -nums[i]);
-        ret.insert(ret.begin(), v.begin(), v.end());
-      }
-      return ret;
-    }
 };
 
 int main(int argc, char const *argv[])
 {
-  vector<int> v{-2, 0, 1, 1, 2};
+  vector<int> v{3, 2, 4};
   Solution s;
-  s.threeSum(v);
+  s.twoSum(v, 6);
   return 0;
 }
 
